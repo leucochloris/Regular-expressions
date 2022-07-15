3b70794863b63c802617186326c8d7c24bb5ab9d
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-07-15 11:55:42 +0300

    Append library pandas

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import re
+import pandas as pd
 
-a = '121245abc456 ABC4545ABC4545151065abn_abc Irina, Olga, Petr, yankov@mail.ru, job@hh.ru, small2@ii.net'
+dump_file = r'C:\Users\a.shesterikov.NKO\Documents\GitHub\Regular-expressions\emails.txt'  # path to dumpfile emails.txt
+input_fule = open(dump_file, mode='r')
+my_text = input_fule.read()
+# print(my_text)    # output all content from emails.txt
 
-pattern = r"@\w+\.\w+"    # at, all alphabet symbol, dot, all alphabet symbol
-matches = re.findall(pattern, a)
 
-for i in matches:
-    print(i)
+pattern = r"@\w+\.\w+"  # at, all alphabet symbol, dot, all alphabet symbol
+matches = re.findall(pattern, my_text)
+
+# for i in matches:
+#     print(i)
+
+print(pd.unique(matches))  # output unique vallues
