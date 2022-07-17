c5ec7bc7753efaaa9a64e603ccfcf2566eb111c5
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-07-17 17:09:06 +0300

    Add agregat function

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import re
 import pandas as pd
 
-dump_file = r'C:\Users\a.shesterikov.NKO\Documents\GitHub\Regular-expressions\emails.txt'  # path to dumpfile emails.txt
+dump_file = r'C:\Users\prove\Documents\GitHub\Regular-expressions\emails.txt'  # path to dumpfile emails.txt -------------------- change to your path
+
 input_fule = open(dump_file, mode='r')
 my_text = input_fule.read()
-# print(my_text)    # output all content from emails.txt
-
+# print(my_text)  # output all content from emails.txt
 
 pattern = r"@\w+\.\w+"  # at, all alphabet symbol, dot, all alphabet symbol
 matches = re.findall(pattern, my_text)
 
-# for i in matches:
-#     print(i)
+dy_dict = {'Domain name': matches}
+df = pd.DataFrame(dy_dict)
+df.groupby('Domain name')
 
-print(pd.unique(matches))  # output unique vallues
+distinct = df.groupby(matches)
+print(distinct.count())
